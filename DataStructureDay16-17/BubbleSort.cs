b9f37a4495f8e878cf25f3b876d7d50bd470e622
patelaftab <10patelaftab@gmail.com>
2023-03-01 21:15:34 +0530

    [Aftab] : Chcking Program For Bubble Sort

--- DataStructureDay16-17/BubbleSort.cs
+++ DataStructureDay16-17/BubbleSort.cs
@@ -9,26 +9,26 @@ namespace DataStructureDay16_17
     public class BubbleSort
     {
         public static void Sort()
-        //{
-        //    int[] array = { 10, 60, 30, 20, 40, 50, 70, 90, 80 };
-        //    int temp = 0;
+        {
+            int[] array = { 10, 60, 30, 20, 40, 50, 70, 90, 80 };
+            int temp = 0;
 
-        //    for (int i = 0; i < array.Length - 1; i++)
-        //    {
-        //        for (int j = 0; j < array.Length - 1; j++)
-        //        {
-        //            if (array[j] > array[j + 1])
-        //            {
-        //                temp = array[j + 1];
-        //                array[j + 1] = array[j];
-        //                array[j] = temp;
-        //            }
-        //        }
-        //    }
-        //    foreach (int i in array)
-        //    {
-        //        Console.WriteLine(i);
-        //    }
-        //}
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        temp = array[j + 1];
+                        array[j + 1] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+            foreach (int i in array)
+            {
+                Console.WriteLine(i);
+            }
+        }
     }
 }
